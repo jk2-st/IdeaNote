f44d2067013d4cd3bccb67748e9eb32c43766329
jk2-st <jka206119@gmail.com>
2023-02-25 18:21:39 +0900

    マテリアルUIrearct18に入らなかった

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -8,16 +8,38 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react';
+
+const Sidebar = () => {
+  return (
+    <Box bg="gray.900" color="white" h="100vh" w="200px" p="4">
+      <Text fontSize="lg" fontWeight="bold">Sidebar</Text>
+    </Box>
+  );
+};
+
+const MainContent = () => {
+  return (
+    <Box >
+      <Text fontSize="lg">Main Content</Text>
+    </Box>
+  );
+};
 
 function App({ signOut }) {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    // <View className="App">
+    //   <Card>
+    //     <Image src={logo} className="App-logo" alt="logo" />
+    //     <Heading level={1}>We now have Auth!</Heading>
+    //   </Card>
+    //   <Button onClick={signOut}>Sign Out</Button>
+    // </View>
+    <Flex>
+    <Sidebar />
+    <MainContent />
+    <Button onClick={signOut}>Sign Out</Button>
+    </Flex>
   );
 }
 
