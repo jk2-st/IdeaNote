834620e960af478cbdacfc5b0f52c23bfb92dfbb
jk2-st <jka206119@gmail.com>
2023-03-11 16:44:27 +0900

    コメント更新削除用のアイコン作成

--- amplifyapp/src/components/CommentList.js
+++ amplifyapp/src/components/CommentList.js
@@ -5,6 +5,9 @@ import List from '@mui/material/List';
 import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import { amber } from '@mui/material/colors';
+import DeleteIcon from '@mui/icons-material/Delete';
+import BuildCircle from '@mui/icons-material/BuildCircle';
+import { ListItemIcon } from '@mui/material';
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL;
 
@@ -32,6 +35,13 @@ export const CommentList = () => {
 
   const comment = useSelector((state) => state.comment);
   console.log('this is comments : ', comment);
+  const isDisplay = false; // TODO: 編集モード時のみ変更削除アイコンを表示する
+  const updateComment = ($id) => {
+    // TODO: コメント更新APIをリクエスト＋編集モード解除
+  };
+  const deleteComment = ($id) => {
+    // TODO: コメント削除APIをリクエスト＋編集モード解除
+  };
   return (
     <Box>
       <List spacing={1} sx={{bgcolor:amber[50]}}>
@@ -41,7 +51,11 @@ export const CommentList = () => {
           <>
             {comment.map(item => (
               <ListItem key={item.entity_id}>
-                <List>{item.comment}</List>
+                <List sx={{ alignItems: "center"}}>{item.comment}</List>
+                <ListItemIcon sx={{ display: isDisplay ? "flex" : "none", justifyContent: "flex-end"}}>
+                  <BuildCircle onClick={updateComment(item.entity_id)}/>
+                  <DeleteIcon onClick={deleteComment(item.entity_id)}/>
+                </ListItemIcon>
               </ListItem>
             ))}
           </>
