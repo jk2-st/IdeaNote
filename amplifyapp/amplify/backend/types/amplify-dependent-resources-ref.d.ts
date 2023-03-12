bb36a0216ed17385ca316c8e79b11dd3560ddfc6
jk2-st <jka206119@gmail.com>
2023-03-12 19:10:28 +0900

    amplifyを再設定

--- amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "amplifyapp542b0499": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
